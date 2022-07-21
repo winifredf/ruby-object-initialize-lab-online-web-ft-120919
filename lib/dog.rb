b1d7111f85dbdacc60ebefdab52f0835140d2bec
winifredf <winifred.frierson@gmail.com>
2022-07-21 19:23:39 -0400

    commit tree 775d9228c1248b182a17c3a1c0c34ff0353c56e6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,32 @@
+# class Dog
+#   def initialize(name, breed = "Mutt")
+#   @name = name
+#   @breed = breed
+# end
+
+# fido = Dog.new("Fido")
+# end
+
+
 class Dog
+  
+  attr_reader :name, :breed
+
   def initialize(name, breed = "Mutt")
-  @name = name
-  @breed = breed
+    @name = name
+    @breed = breed
+  end
 end
 
-fido = Dog.new("Fido")
-end
+
+
+
+
+
+
+
+
+
+
+
+
